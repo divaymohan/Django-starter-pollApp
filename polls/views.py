078e43d7ff6394bd4560174e59881382f80ef89a
divaymohan <divaymohan05@gmail.com>
2019-09-18 09:06:47 +0530

    adding question model

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, World. You are at the polls index.")
+
+# Create your views here.
