e3df2411cdd04fe06d69c38fbdb0b124dd83f912
divaymohan <divaymohan05@gmail.com>
2019-09-18 12:43:37 +0530

    adding views results ,vote , details and understanding the url config

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.details, name='details'),
+    # ex: /polls/5/result/
+    path('<int:question_id>/results/', views.result, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,21 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, World. You are at the polls index.")
 
 # Create your views here.
+
+
+def details(request, question_id):
+    return HttpResponse("You'are looking at questtion %s." % question_id)
+
+
+def result(request, question_id):
+    response = "You are looking at the result of Question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on question %s." % question_id)
